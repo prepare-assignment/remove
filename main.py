8c0f9f0b78941ae96a49e44614e3de1841a03e7d
Bonajo <m.bonajo@fontys.nl>
2023-09-19 19:08:04 +0200

    feat: update action to be in line with mv command

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from prepare_toolbox.file import get_matching_files
 
 def remove() -> None:
     try:
+        # glob(s) to match
         inputs = get_input("input")
+        # ignore nonexistent files and arguments
         force = get_input("force")
+        # remove directories and their contents recursively
         recursive = get_input("recursive")
 
-        for path in inputs:
-            if os.path.isdir(path):
-                if recursive:
-                    if force:
-                        shutil.rmtree(path)
-                    else:
-                        os.rmdir(path)
+        for glob in inputs:
+            files = get_matching_files(glob, excluded=None, relative_to=None, recursive=recursive)
+            if len(files) == 0 and not force:
+                raise AssertionError(f"No files match {glob} and force is false")
+            for path in files:
+                if os.path.isdir(path):
+                    if not recursive:
+                        raise AssertionError(f"{path} is a directory and recursive if false")
+                    shutil.rmtree(path)
                 else:
-                    raise AssertionError(f"Path {path} is a directory but recursive is not set")
-            else:
-                files = get_matching_files(path)
-                for file in files:
-                    os.remove(file)
-
+                    os.remove(path)
     except Exception as e:
         print(e)
         exit(1)
