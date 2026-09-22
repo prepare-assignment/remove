81159aca1c2730d45d0d452aea055143647968c9
Bonajo <m.bonajo@fontys.nl>
2026-09-22 10:29:12 +0200

    refactor: move main.py into the prepare_remove package

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
 import prepare_toolbox.core
 import pytest
 
-import main
-from main import remove
+import prepare_remove.main as main
+from prepare_remove.main import remove
 from pytest_mock import MockerFixture
 
 
@@ -11,7 +11,7 @@ def test_no_globs(mocker: MockerFixture):
         if key == "input":
             return []
         return False
-    mocker.patch('main.get_input', side_effect=__get_input)
+    mocker.patch('prepare_remove.main.get_input', side_effect=__get_input)
     spy = mocker.spy(main, "set_output")
     remove()
     spy.assert_called_once_with("files", [])
@@ -22,8 +22,8 @@ def test_no_force(mocker: MockerFixture):
         if key == "input":
             return ["test"]
         return False
-    mocker.patch('main.get_input', side_effect=__get_input)
-    mocker.patch("main.get_matching_files", return_value=[])
+    mocker.patch('prepare_remove.main.get_input', side_effect=__get_input)
+    mocker.patch("prepare_remove.main.get_matching_files", return_value=[])
     spy = mocker.spy(main, "set_failed")
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         remove()
@@ -36,8 +36,8 @@ def test_no_recursive(mocker: MockerFixture):
         if key == "input":
             return ["test"]
         return False
-    mocker.patch('main.get_input', side_effect=__get_input)
-    mocker.patch("main.get_matching_files", return_value=["file"])
+    mocker.patch('prepare_remove.main.get_input', side_effect=__get_input)
+    mocker.patch("prepare_remove.main.get_matching_files", return_value=["file"])
     mocker.patch("os.path.isdir", return_value=True)
     spy = mocker.spy(main, "set_failed")
     with pytest.raises(SystemExit) as pytest_wrapped_e:
@@ -54,8 +54,8 @@ def test_successful(mocker: MockerFixture):
 
     def __is_dir(path: str):
         return True if path == "dir" else False
-    mocker.patch('main.get_input', side_effect=__get_input)
-    mocker.patch("main.get_matching_files", return_value=["file", "dir"])
+    mocker.patch('prepare_remove.main.get_input', side_effect=__get_input)
+    mocker.patch("prepare_remove.main.get_matching_files", return_value=["file", "dir"])
     mocker.patch("os.path.isdir", side_effect=__is_dir)
     mocked_rmtree = mocker.patch("shutil.rmtree")
     mocked_remove = mocker.patch("os.remove")
@@ -71,7 +71,7 @@ def test_successful(mocker: MockerFixture):
 def test_exception(mocker: MockerFixture):
     def __get_input(key: str):
         raise Exception("Test")
-    mocker.patch('main.get_input', side_effect=__get_input)
+    mocker.patch('prepare_remove.main.get_input', side_effect=__get_input)
     spy = mocker.spy(main, "set_failed")
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         remove()
